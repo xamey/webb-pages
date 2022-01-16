15cba6e84507927a7402a99876055d38f24f06fb
xamey <ecallemax@gmail.com>
2022-01-16 14:01:15 +0100

    Added react-moralis configuration

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="seDzjwqKL2cqaeh2KCcuMmaDgSfxVjqHcdyNUhvJ" serverUrl="https://ypfhsrjupfcg.usemoralis.com:2053/server">
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
