63b62d7030e2f0a829ea23c286b33c6043028e06
xamey <ecallemax@gmail.com>
2022-01-17 12:41:08 +0100

    Fixed login, needed to ensure that web3 is enabled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useMoralis } from "react-moralis";
 import { SearchContext } from "../../hooks/SearchProvider";
 import "./Login.css";
 
 export default function Login() {
-  const { authenticate, isAuthenticated, user, logout } = useMoralis();
+  const {
+    authenticate,
+    enableWeb3,
+    isWeb3Enabled,
+    isAuthenticated,
+    user,
+    logout,
+  } = useMoralis();
+
+  useEffect(() => {
+    if (!isWeb3Enabled) {
+      enableWeb3();
+    }
+  }, [isWeb3Enabled, enableWeb3]);
   const { setSearch } = useContext(SearchContext);
   if (!isAuthenticated) {
     return (
       <div>
-        <div className="login login-btn green-text" onClick={() => authenticate()}>
+        <div
+          className="login login-btn green-text"
+          onClick={() => authenticate()}
+        >
           authenticate...
         </div>
       </div>
@@ -20,12 +36,16 @@ export default function Login() {
     return (
       <div className="login connected">
         <div className="address">
-        {ethAddress.slice(0, 6)}...
-        {ethAddress.slice(ethAddress.length - 4, ethAddress.length)}
+          {ethAddress.slice(0, 6)}...
+          {ethAddress.slice(ethAddress.length - 4, ethAddress.length)}
         </div>
         <div className="actions green-text">
-          <div className="action" onClick={() => logout()}>logout</div>
-          <div className="action" onClick={() => setSearch(ethAddress)}>my appartments</div>
+          <div className="action" onClick={() => logout()}>
+            logout
+          </div>
+          <div className="action" onClick={() => setSearch(ethAddress)}>
+            my appartments
+          </div>
         </div>
       </div>
     );
